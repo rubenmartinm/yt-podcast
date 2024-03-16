2ce8a8caaccc5b7117eb91896c13faea5df4ad74
Ruben Martin <rmmonteagudo@gmail.com>
2024-03-16 14:02:37 +0100

    feat: revert to previus approach for date

--- main.py
+++ main.py
@@ -40,12 +40,6 @@ def generate_podcast_feed(youtube_channels):
                 video_info = yaml.safe_load(video)
                 video_url = video_info['url']
                 title = video_info['title']
-                output_date = subprocess.check_output(['yt-dlp', '--print upload_date', video_url])
-                upload_date = output_date.strip()  # Elimina cualquier espacio en blanco alrededor de la fecha
-                # Formato de fecha devuelto por yt-dlp
-                upload_date_format = '%Y%m%d'
-                upload_datetime = datetime.strptime(upload_date, upload_date_format)
-
                 # Normalizar y reemplazar caracteres especiales en español
                 title = unicodedata.normalize('NFKD', title).encode('ASCII', 'ignore').decode('ASCII')
                 # Formatear el título para una URL válida
@@ -71,8 +65,9 @@ def generate_podcast_feed(youtube_channels):
                 fe.link(href=video_url)
                 fe.description(title)
                 fe.enclosure(url=audio_url, length='123456', type='audio/mpeg')
-                fe.pubDate(upload_datetime.strftime('%a, %d %b %Y %H:%M:%S %z'))
-                
+                pub_date = datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S %z')  # Formato de fecha requerido
+                fe.pubDate(pub_date)
+
         except subprocess.CalledProcessError as e:
             print(f'Error al ejecutar yt-dlp: {e}')
             sys.exit(1)
@@ -90,5 +85,7 @@ if __name__ == '__main__':
     config_file = '/data/yt_channels.yaml'
     with open(config_file, 'r') as f:
         youtube_channels = yaml.safe_load(f)
+    
+    generate_podcast_feed(youtube_channels['podcasts'])
     
     generate_podcast_feed(youtube_channels['podcasts'])
